9bd3c11ce2d38592a7b41b4aa81f62fcebbaaabf
jdquimbiulco <juandiegoq21@gmail.com>
2024-07-17 22:46:09 -0500

    Banlance tree implementation

--- ABB/ABB.h
+++ ABB/ABB.h
@@ -1,50 +1,126 @@
+#ifndef ABB_H
+#define ABB_H
+
 #include <iostream>
 
-struct node {
-    int key;
-    struct node *left, *right;
+struct nodo {
+    int clave;
+    struct nodo *izquierda, *derecha;
+    int altura;
 };
- 
- 
 
+int altura(struct nodo *N) {
+    if (N == nullptr)
+        return 0;
+    return N->altura;
+}
 
- struct node* newNode(int item)
- {
-     struct node* temp
-         = new struct node;
-     temp->key = item;
-     temp->left = temp->right = nullptr;
-     return temp;
- }
-  
- 
- struct node* insert(struct node* node, int key)
- {
-     // If the tree is empty, return a new node
-     if (node == nullptr)
-         return newNode(key);
-  
-     // Otherwise, recur down the tree
-     if (key < node->key)
-         node->left = insert(node->left, key);
-     else if (key > node->key)
-         node->right = insert(node->right, key);
-  
-     // Return the (unchanged) node pointer
-     return node;
- }
-  
+struct nodo* nuevoNodo(int clave) {
+    struct nodo* nodo = new struct nodo;
+    nodo->clave = clave;
+    nodo->izquierda = nodo->derecha = nullptr;
+    nodo->altura = 1; // nuevo nodo se añade al final del árbol
+    return nodo;
+}
 
- struct node* search(struct node* root, int key)
- {
-     // Base Cases: root is null or key is present at root
-     if (root == nullptr || root->key == key)
-         return root;
-  
-     // Key is greater than root's key
-     if (root->key < key)
-         return search(root->right, key);
-  
-     // Key is smaller than root's key
-     return search(root->left, key);
- }
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+struct nodo* rotacionDerecha(struct nodo *y) {
+    struct nodo *x = y->izquierda;
+    struct nodo *T2 = x->derecha;
+
+    // Realizar rotación
+    x->derecha = y;
+    y->izquierda = T2;
+
+    // Actualizar alturas
+    y->altura = max(altura(y->izquierda), altura(y->derecha)) + 1;
+    x->altura = max(altura(x->izquierda), altura(x->derecha)) + 1;
+
+    // Devolver nueva raíz
+    return x;
+}
+
+struct nodo* rotacionIzquierda(struct nodo *x) {
+    struct nodo *y = x->derecha;
+    struct nodo *T2 = y->izquierda;
+
+    // Realizar rotación
+    y->izquierda = x;
+    x->derecha = T2;
+
+    // Actualizar alturas
+    x->altura = max(altura(x->izquierda), altura(x->derecha)) + 1;
+    y->altura = max(altura(y->izquierda), altura(y->derecha)) + 1;
+
+    // Devolver nueva raíz
+    return y;
+}
+
+// Obtener el factor de balanceo de un nodo N
+int obtenerBalance(struct nodo *N) {
+    if (N == nullptr)
+        return 0;
+    return altura(N->izquierda) - altura(N->derecha);
+}
+
+struct nodo* insertar(struct nodo* nodo, int clave) {
+    // 1. Realizar la inserción normal en el BST
+    if (nodo == nullptr)
+        return nuevoNodo(clave);
+
+    if (clave < nodo->clave)
+        nodo->izquierda = insertar(nodo->izquierda, clave);
+    else if (clave > nodo->clave)
+        nodo->derecha = insertar(nodo->derecha, clave);
+    else // Las claves iguales no se permiten en el BST
+        return nodo;
+
+    // 2. Actualizar la altura del nodo ancestro
+    nodo->altura = 1 + max(altura(nodo->izquierda), altura(nodo->derecha));
+
+    // 3. Obtener el factor de balanceo de este nodo ancestro para verificar si se desbalanceó
+    int balance = obtenerBalance(nodo);
+
+    // Si el nodo se desbalanceó, entonces hay 4 casos
+
+    // Caso izquierda-izquierda
+    if (balance > 1 && clave < nodo->izquierda->clave)
+        return rotacionDerecha(nodo);
+
+    // Caso derecha-derecha
+    if (balance < -1 && clave > nodo->derecha->clave)
+        return rotacionIzquierda(nodo);
+
+    // Caso izquierda-derecha
+    if (balance > 1 && clave > nodo->izquierda->clave) {
+        nodo->izquierda = rotacionIzquierda(nodo->izquierda);
+        return rotacionDerecha(nodo);
+    }
+
+    // Caso derecha-izquierda
+    if (balance < -1 && clave < nodo->derecha->clave) {
+        nodo->derecha = rotacionDerecha(nodo->derecha);
+        return rotacionIzquierda(nodo);
+    }
+
+    // Devolver el puntero del nodo (sin cambios)
+    return nodo;
+}
+
+struct nodo* buscar(struct nodo* raiz, int clave) {
+    // Casos base: raiz es null o la clave está presente en la raiz
+    if (raiz == nullptr || raiz->clave == clave)
+        return raiz;
+
+    // La clave es mayor que la clave de la raiz
+    if (raiz->clave < clave)
+        return buscar(raiz->derecha, clave);
+
+    // La clave es menor que la clave de la raiz
+    return buscar(raiz->izquierda, clave);
+}
+
+#endif
--- ABB/main.cpp
+++ ABB/main.cpp
@@ -7,26 +7,26 @@ void mostrarMenu();
 int main()
 {
     int opcion;
-    struct node* root = nullptr;
-    root = insert(root, 50);
-    insert(root, 30);
-    insert(root, 20);
-    insert(root, 40);
-    insert(root, 70);
-    insert(root, 60);
-    insert(root, 80);
+    struct nodo* root = nullptr;
+    root = insertar(root, 50);
+    insertar(root, 30);
+    insertar(root, 20);
+    insertar(root, 40);
+    insertar(root, 70);
+    insertar(root, 60);
+    insertar(root, 80);
 
 
     int key = 6;
 
-    if (search(root, key) == nullptr)
+    if (buscar(root, key) == nullptr)
         cout << key << " No Encontrado" << endl;
     else
         cout << key << " Encontrado" << endl;
 
     key = 60;
 
-    if (search(root, key) == nullptr)
+    if (buscar(root, key) == nullptr)
         cout << key << " No Encomtrado" << endl;
     else
         cout << key << " Econtrado" << endl;
